e727eed13137cdec68027662ab2ac30d874e63a4
alenkoikkara <alendennis77@gmail.com>
2026-04-17 14:10:03 -0500

    updating fonts

--- src/components/FluidCanvas.jsx
+++ src/components/FluidCanvas.jsx
@@ -1,12 +1,12 @@
-import React, { Suspense, useRef, useMemo } from 'react';
+import React, { Suspense, useRef, useMemo, useState, useEffect } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { Text, Center, ScrollControls, Scroll, useScroll, Image } from '@react-three/drei';
 import { Fluid } from '@whatisjery/react-fluid-distortion';
 import { EffectComposer } from '@react-three/postprocessing';
 
 // Font URLs
-const SATOSHI_BLACK = "https://cdn.fontshare.com/wf/NHPGVFYUXYXE33DZ75OIT4JFGHITX5PE/PSUTMASCDJTVPERDYJZPN23BVUFUCQIF/J64QX5IPOHK56I2KYUNBQ5M2XWZEYKYX.ttf";
-const NEWSREADER_URL = "https://fonts.gstatic.com/s/newsreader/v26/cY9qfjOCX1hbuyalUrK49dLac06G1ZGsZBtoBCzBDXXD9JVF438weI_ADA.ttf";
+const SATOSHI_BOLD = "https://cdn.fontshare.com/wf/LAFFD4SDUCDVQEXFPDC7C53EQ4ZELWQI/PXCT3G6LO6ICM5I3NTYENYPWJAECAWDD/GHM6WVH6MILNYOOCXHXB5GTSGNTMGXZR.ttf";
+const NEWSREADER_BOLD = "https://fonts.gstatic.com/s/newsreader/v26/cY9qfjOCX1hbuyalUrK49dLac06G1ZGsZBtoBCzBDXXD9JVF438wn4jADA.ttf";
 const PIXELIFY_URL = "https://fonts.gstatic.com/s/pixelifysans/v3/CHy2V-3HFUT7aC4iv1TxGDR9DHEserHN25py2TQO131Y.ttf";
 
 // Asset Imports
@@ -19,6 +19,16 @@ function Scene() {
   const scroll = useScroll();
   const fluidRef = useRef();
   const { viewport } = useThree();
+  const [p1Width, setP1Width] = useState(0);
+  const [p2Width, setP2Width] = useState(0);
+  const [useSatoshiForO, setUseSatoshiForO] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setUseSatoshiForO(prev => !prev);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   useFrame((state, delta) => {
     if (fluidRef.current) {
@@ -37,30 +47,67 @@ function Scene() {
 
       <Scroll>
         {/* Section 1: Hero Text */}
-        <Center top position={[0, 0, -10]}>
+        <Center key={viewport.width} position={[-1.5, .4, -2]}>
           <Text
-            font={SATOSHI_BLACK}
-            fontSize={1.2}
+            font={SATOSHI_BOLD}
+            fontSize={.36}
             color="black"
-            anchorX="center"
+            anchorX="left"
             anchorY="middle"
+            textAlign="left"
+            position={[0, 1, 0]}
           >
-            Hi
+            Hi !
           </Text>
-        </Center>
-
-        <Center bottom position={[0, -1.5, -10]}>
           <Text
-            font={NEWSREADER_URL}
-            fontSize={0.8}
+            font={NEWSREADER_BOLD}
+            fontSize={.64}
             color="black"
-            anchorX="center"
+            anchorX="left"
             anchorY="middle"
-            maxWidth={viewport.width * 0.8}
-            textAlign="center"
+            textAlign="left"
+            position={[0, .4, 0]}
           >
-            I am Alen Koikkara. I Like to create.
+            I am Alen Koikkara.
           </Text>
+          <group position={[0, -0.2, 0]}>
+            <Text
+              font={SATOSHI_BOLD}
+              fontSize={0.48}
+              color="#10110E"
+              anchorX="left"
+              anchorY="middle"
+              textAlign="left"
+              onSync={(m) => {
+                m.geometry.computeBoundingBox();
+                setP1Width(m.geometry.boundingBox.max.x - 0.01);
+              }}
+            >
+              I like t
+            </Text>
+            <Text
+              font={useSatoshiForO ? SATOSHI_BOLD : PIXELIFY_URL}
+              fontSize={useSatoshiForO ? 0.40 : 0.42}
+              color="black"
+              anchorX="left"
+              anchorY="middle"
+              textAlign="left"
+              position={[p1Width, useSatoshiForO ? -0.04 : -0.05, 0]}
+            >
+              o
+            </Text>
+            <Text
+              font={SATOSHI_BOLD}
+              fontSize={0.48}
+              color="black"
+              anchorX="left"
+              anchorY="middle"
+              textAlign="left"
+              position={[p1Width + p2Width + 0.35, -.01, 0]}
+            >
+              create.
+            </Text>
+          </group>
         </Center>
 
         {/* Section 2: Architecture */}
@@ -106,10 +153,10 @@ function Scene() {
           intensity={0.5}
           force={1}
           distortion={0.1}
-          curl={0.1}
+          curl={0.2}
           radius={0.25}
-          swirl={1}
-          blend={1}
+          swirl={2}
+          blend={2}
           showBackground={false}
           fluidColor="#000000"
         />
